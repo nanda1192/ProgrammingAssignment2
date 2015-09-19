083e8f5dcf3076c45222fdd14233917428cf7b8e
nanda1192 <nandakumar911@gmail.com>
2015-09-19 16:04:20 +0530

    WHatevzz

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,40 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Hi there I am Nanda.
+##So,the function 'makeCacheMatrix' helps set and retrieve values for the
+##input matrix and its inverse.
 
-## Write a short comment describing this function
+## Unlike the example provided,I have calculated
+##the inverse and not just set it to a value.
 
-makeCacheMatrix <- function(x = matrix()) {
-
+makeCacheMatrix<-function(x = matrix()){
+  inv_mtrx <- matrix()
+  set<-function(y = matrix()){
+    x <<- y
+    inv_mtrx <<- NULL
+  }
+  get<-function(){
+    x
+  }
+  setinv <- function(){
+    inv_mtrx <<- solve(x,matrix(rep(1,nrow(x)*ncol(x)),nrow(x),ncol(x)))
+  }
+  getinv <- function(){
+    inv_mtrx
+  }
+  list(set=set,get=get,setinv=setinv,getinv=getinv)
 }
 
 
-## Write a short comment describing this function
+##The function 'cacheSolve' retrieves
+##the inverse if not null,otherwise calculates and returns it.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+cacheSolve<-function(z1,...){
+  invmtrx <- z1$getinv()
+  if(!is.null(invmtrx)){
+    print("Fetching matrix inverse:")
+    return(invmtrx)
+  }
+  else{
+    invmtrx <- z1$setinv()
+  }
+  invmtrx
 }
